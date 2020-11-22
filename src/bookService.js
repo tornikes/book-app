7a79dc0cbc518142d2c6d43f0b6d302f6647c343
tornikes <t.sadghobelashvili@gmail.com>
2020-11-22 13:31:57 +0400

    feat: persist state & change base url

--- src/bookService.js
+++ src/bookService.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3001/data' // 'https://www.googleapis.com/books/v1/volumes?q=programming';
+const baseUrl = 'https://www.googleapis.com/books/v1/volumes?q=programming';
 
 async function fetchBooks() {
     const response = await fetch(baseUrl);
--- src/store.js
+++ src/store.js
@@ -2,13 +2,22 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import booksReducer from './reducers/booksReducer';
 import favoritesReducer from './reducers/favoritesReducer';
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
+
+const persistedState = loadState();
 
 const store = createStore(
     combineReducers({ 
         books: booksReducer,
         favorites: favoritesReducer
     }),
+    persistedState,
     applyMiddleware(thunk)
 );
+
+store.subscribe(throttle(() => {
+    saveState(store.getState())
+}, 1000));
 
 export default store;
